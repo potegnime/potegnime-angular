458fb515e399a5c29936ec33caa072c46d560e4c
Lan <lanlebar5@gmail.com>
2025-02-28 15:47:15 +0100

    improve donate page

--- src/app/modules/about/components/donate-page/donate-page.component.ts
+++ src/app/modules/about/components/donate-page/donate-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/modules/auth/services/auth-service/auth.service';
+import { timingConst } from 'src/app/modules/shared/enums/toastr-timing.enum';
 
 @Component({
   selector: 'app-donate-page',
@@ -6,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./donate-page.component.scss']
 })
 export class DonatePageComponent {
+  protected isLoggedIn: boolean = false;
+  protected walletAddresses = {
+    'btc': 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    'eth': 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',
+    'ltc': 'cccccccccccccccccccccccccccccccccccccccc',
+    'xmr': 'dddddddddddddddddddddddddddddddddddddddd'
+  }
 
+  constructor(
+    private readonly toastr: ToastrService,
+    private readonly router: Router,
+    private readonly authService: AuthService
+  ) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.verifyToken();
+  }
+
+  protected copyAddress(key: 'btc' | 'eth' | 'ltc' | 'xmr'): void {
+    const address: string = this.walletAddresses[key];
+    navigator.clipboard.writeText(address);
+    this.toastr.success('', `${key.toUpperCase()} naslov kopiran!`, { timeOut: timingConst.success });
+  }
 }
