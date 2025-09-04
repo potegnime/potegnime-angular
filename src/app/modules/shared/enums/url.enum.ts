6db8e4b3e478c8cf093dc5661c7c5caedaed5f83
Lan <lanlebar5@gmail.com>
2025-09-05 00:07:57 +0200

    cors

--- src/app/modules/shared/enums/url.enum.ts
+++ src/app/modules/shared/enums/url.enum.ts
@@ -1,5 +1,5 @@
 export const urlConst = {
     appBase: 'https://potegni.me/',
     apiBase: 'https://potegnime-api.onrender.com/api',
-    scraperBase: 'https://potegnime-scraper.onrender.com/',
+    scraperBase: 'https://potegnime-scraper.onrender.com',
 };
