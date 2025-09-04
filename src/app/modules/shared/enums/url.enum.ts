a57e1d5d71a3ff5f92fcdca49ef5866ee7a7a026
Lan <lanlebar5@gmail.com>
2025-09-04 20:50:55 +0200

    prod urls

--- src/app/modules/shared/enums/url.enum.ts
+++ src/app/modules/shared/enums/url.enum.ts
@@ -1,4 +1,4 @@
 export const urlConst = {
-    appBase: 'http://localhost:4200/',
-    apiBase: 'http://localhost:5194/api'
+    appBase: 'https://potegni.me/',
+    apiBase: 'http://localhost:8080/api'
 };
