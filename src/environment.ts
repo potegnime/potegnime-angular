ae30e7ee12a5f86c6335b24ce4713ead08ec7843
Lan <lanlebar6@gmail.com>
2025-12-25 17:43:44 +0100

    document environment.ts

--- src/environment.ts
+++ src/environment.ts
@@ -3,9 +3,11 @@ export const environment = {
    * Production mode flag - used for switching between dev and prod config.json settings
    */
   production: true,
-  
+
   /**
-   * Danger!! When true, the site is under maintenance mode and becomes unavailable
+   * Danger!! When true, the site is in maintenance mode and becomes unavailable,
+   * Frontend will only allow /maintenance page to be displayed, APIs will continue to work normally.
+   * Any users that visit site while in maintenance mode will be logged out.
    */
   underMaintenance: false
 };
