8e087ea2180fc4afed186bbda20b0ba4218b3716
Lan <lanlebar6@gmail.com>
2025-12-30 19:03:18 +0100

    bumbar

--- src/environment.ts
+++ src/environment.ts
@@ -2,14 +2,14 @@ const environment = {
   /**
    * Production mode flag - used for switching between dev and prod config.json settings
    */
-  production: false,
+  production: true,
 
   /**
    * Danger!! When true, the site is in maintenance mode and becomes unavailable,
    * Frontend will only allow /maintenance page to be displayed, APIs will continue to work normally.
    * Any users that visit site while in maintenance mode will be logged out.
    */
-  underMaintenance: false
+  underMaintenance: true
 };
 
 
