ec9d87fecd39e4d0047b7dfe1549c4efdb569091
Lan <lanlebar6@gmail.com>
2026-01-05 11:56:34 +0100

    /u shows 404

--- src/app/features/user/user.routes.ts
+++ src/app/features/user/user.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from '@core/guards/auth.guard';
 
 export const USER_ROUTES: Routes = [
   {
     path: ':username',
     loadComponent: () => import('./components/user-page/user-page.component').then((m) => m.UserPageComponent)
+  },
+  // potegni.me/u/ - 404
+  {
+    path: '**',
+    loadComponent: () => import('@shared/components/error/error.component').then((m) => m.ErrorComponent),
+    canActivate: [AuthGuard]
   }
 ];
