4c21888bad74dcecb45d12d08c4a7fc941dd72a4
Lan <lanlebar6@gmail.com>
2026-01-09 14:45:52 +0100

    hide toasts in maintenance mode

--- src/app/core/services/toast/toast.service.ts
+++ src/app/core/services/toast/toast.service.ts
@@ -1,5 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { underMaintenance } from 'src/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,19 +9,24 @@ export class ToastService {
   private readonly toastr = inject(ToastrService);
   private readonly timingConst = 4000;
 
+  // APIs may be down during maintenance, so we disable toasts in maintenance mode
   public showSuccess(message: string, title?: string): void {
+    if (underMaintenance) return;
     this.toastr.success(message, title, { timeOut: this.timingConst });
   }
 
   public showError(message: string, title?: string): void {
+    if (underMaintenance) return;
     this.toastr.error(message, title, { timeOut: this.timingConst });
   }
 
   public showWarning(message: string, title?: string): void {
+    if (underMaintenance) return;
     this.toastr.warning(message, title, { timeOut: this.timingConst });
   }
 
   public showInfo(message: string, title?: string): void {
+    if (underMaintenance) return;
     this.toastr.info(message, title, { timeOut: this.timingConst });
   }
 }
