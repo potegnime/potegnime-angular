5fcbb2aa4dcb9ebc50bb8456d0be8dd815509075
Lan <lanlebar6@gmail.com>
2026-08-05 17:14:15 +0200

    update route guards

--- src/app/features/about/about.routes.ts
+++ src/app/features/about/about.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from '@core/guards/auth.guard';
 
 export const ABOUT_ROUTES: Routes = [
   {
     path: 'about',
     loadComponent: () => import('./components/about-page/about-page.component').then((m) => m.AboutPageComponent),
-    pathMatch: 'full'
+    pathMatch: 'full',
+    canActivate: [AuthGuard]
   },
   {
     path: 'donate',
